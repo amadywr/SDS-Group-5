9a5c12dcd7e973a29d0e105d000b49b996c29a60
Ahmad Yawari <k_han21@yahoo.com>
2022-10-11 00:45:27 +1100

    changed results page and changed recommendation page

--- app.js
+++ app.js
@@ -1,31 +1,37 @@
-const express = require("express")
-const path = require("path")
-const ejsMate = require("ejs-mate")
+const express = require('express')
+const path = require('path')
+const ejsMate = require('ejs-mate')
 
 const app = express()
 const port = 3000
 
-app.engine("ejs", ejsMate)
-app.set("view engine", "ejs")
-app.set("views", path.join(__dirname, "views"))
+app.engine('ejs', ejsMate)
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'views'))
 app.use(express.urlencoded({ extended: true }))
-app.use(express.static(path.join(__dirname, "public")))
+app.use(express.static(path.join(__dirname, 'public')))
 
-app.get("/", (req, res) => {
-  res.render("homepage")
+app.get('/', (req, res) => {
+  res.render('homepage')
 })
 
-app.get("/login", (req, res) => {
-  res.render("login")
+app.get('/login', (req, res) => {
+  res.render('login')
 })
 
-app.get("/recommender", (req, res) => {
-  res.render("recommender")
+app.get('/recommender', (req, res) => {
+  res.render('recommender')
 })
 
-app.post("/recommender", (req, res) => {
-  console.log(req.body)
-  res.json(req.body)
+app.post('/recommender', (req, res) => {
+  const userSelection = Object.values(req.body)
+  console.log(userSelection)
+
+  if (userSelection.includes('medicine')) {
+    console.log('we recommond nursing')
+  }
+
+  res.render('result', { recommendedCourses: userSelection })
 })
 
 app.listen(port, () => {
