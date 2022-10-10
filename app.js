b596d5b2c93f8a593ff0c6b7f684673ed3e16c01
Ahmad <k_han21@yahoo.com>
2022-10-10 23:58:37 +1100

    added more pages and routes

--- app.js
+++ app.js
@@ -1,18 +1,31 @@
-const express = require('express')
-const path = require('path')
-const ejsMate = require('ejs-mate')
+const express = require("express")
+const path = require("path")
+const ejsMate = require("ejs-mate")
 
 const app = express()
 const port = 3000
 
-app.engine('ejs', ejsMate)
-app.set('view engine', 'ejs')
-app.set('views', path.join(__dirname, 'views'))
+app.engine("ejs", ejsMate)
+app.set("view engine", "ejs")
+app.set("views", path.join(__dirname, "views"))
 app.use(express.urlencoded({ extended: true }))
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, "public")))
 
-app.get('/', (req, res) => {
-  res.render('homepage')
+app.get("/", (req, res) => {
+  res.render("homepage")
+})
+
+app.get("/login", (req, res) => {
+  res.render("login")
+})
+
+app.get("/recommender", (req, res) => {
+  res.render("recommender")
+})
+
+app.post("/recommender", (req, res) => {
+  console.log(req.body)
+  res.json(req.body)
 })
 
 app.listen(port, () => {
--- public/javascript/index.js
+++ public/javascript/index.js
@@ -1,1 +1,34 @@
-console.log('hello')
+const buttons = document.querySelectorAll(".rc-poi-buttons")
+const clear = document.querySelector("#btn-clear")
+const search = document.querySelector("#btn-search")
+const ul = document.querySelector(".rc-list-selected")
+
+buttons.forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    let doesExists = false
+    const lis = ul.children
+
+    for (let i = 0; i < lis.length; i++) {
+      if (e.target.innerText === lis[i].innerText) {
+        console.log("already exists", i)
+        doesExists = true
+      }
+    }
+
+    if (doesExists === false) {
+      const li = document.createElement("li")
+      li.innerText = e.target.innerText
+      ul.appendChild(li)
+    }
+  })
+})
+
+clear.addEventListener("click", () => {
+  ul.innerHTML = ""
+})
+
+search.addEventListener("click", () => {
+  for (let i = 0; i < ul.children.length; i++) {
+    console.log(ul.children[i].innerText)
+  }
+})
