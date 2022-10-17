ef0d172a4ac3422275b4e25d1d9d2d34e7852517
Ahmad <k_han21@yahoo.com>
2022-10-18 10:49:33 +1100

    reset

--- app.js
+++ app.js
@@ -1,55 +1,15 @@
-const express = require('express')
-const path = require('path')
-const ejsMate = require('ejs-mate')
-const courseRecommender = require('./courseRecommender')
+const express = require("express")
+const path = require("path")
+const ejsMate = require("ejs-mate")
 
 const app = express()
 const port = 3000
 
-app.engine('ejs', ejsMate)
-app.set('view engine', 'ejs')
-app.set('views', path.join(__dirname, 'views'))
+app.engine("ejs", ejsMate)
+app.set("view engine", "ejs")
+app.set("views", path.join(__dirname, "views"))
 app.use(express.urlencoded({ extended: true }))
-app.use(express.static(path.join(__dirname, 'public')))
-
-const fields = [
-  'Maths',
-  'IT',
-  'Medicine',
-  'Anthropology',
-  'History',
-  'Law',
-  'Languages',
-  'Philosophy',
-  'Religion',
-  'Economics',
-  'Art',
-]
-
-app.get('/', (req, res) => {
-  res.render('homepage')
-})
-
-app.get('/register', (req, res) => {
-  res.render('register')
-})
-
-app.get('/login', (req, res) => {
-  res.render('login')
-})
-
-app.get('/recommender', (req, res) => {
-  res.render('recommender', { fields })
-})
-
-app.post('/recommender', (req, res) => {
-  const userSelection = Object.values(req.body)
-  console.log(req.body)
-
-  // console.log('from func: ', courseRecommender(userSelection))
-
-  res.render('result', { recommendedCourses: courseRecommender(userSelection) })
-})
+app.use(express.static(path.join(__dirname, "public")))
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
