976d6fe3598783bbf2dc3cfa579e2dd9d5974c91
Ahmad <k_han21@yahoo.com>
2022-10-18 11:04:26 +1100

    added admin login page

--- app.js
+++ app.js
@@ -15,6 +15,10 @@ app.get("/", (req, res) => {
   res.render("homepage")
 })
 
+app.get("/login", (req, res) => {
+  res.render("login")
+})
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
